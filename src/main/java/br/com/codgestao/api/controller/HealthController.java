90d064ddaa58c7156e783d2f46c32d7e5cfbf1e7
Leonardo Freitas <leonardofrosario@hotmail.com>
2025-08-19 08:50:01 -0300

    Adiciona endpoint /health para teste de status da API

---
+++ src/main/java/br/com/codgestao/api/controller/HealthController.java
@@ -0,0 +1,12 @@
+package br.com.codgestao.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "API rodando com sucesso 🚀";
+    }
+}
